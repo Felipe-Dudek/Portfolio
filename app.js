699a7318d9c58aae03977d54e0a8240c0a4287ae
Felipe-Dudek <82465554+Felipe-Dudek@users.noreply.github.com>
2024-05-02 21:47:15 -0300

    New repository with my portfolio
    These are the basic files of my portfolio

---
+++ app.js
@@ -0,0 +1,19 @@
+function scrollMinhasExperiencias(){
+    const minhasExperiencias = document.getElementById("minhas-experiencias");
+    minhasExperiencias.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollMinhasExperiencias(){
+    const minhasExperiencias = document.getElementById("minhas-experiencias");
+    minhasExperiencias.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollMinhasExperiencias(){
+    const minhasExperiencias = document.getElementById("minhas-experiencias");
+    minhasExperiencias.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollMinhasExperiencias(){
+    const minhasExperiencias = document.getElementById("minhas-experiencias");
+    minhasExperiencias.scrollIntoView({ behavior: "smooth" });
+}
